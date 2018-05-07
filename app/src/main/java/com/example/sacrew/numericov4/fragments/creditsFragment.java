d28dedcc41301225ea116d1dd23064bb0f87db9b
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2018-05-06 19:11:49 -0500

    Añadiendo los métodos de Pivoteo Parcial y Total

---
+++ app/src/main/java/com/example/sacrew/numericov4/fragments/creditsFragment.java
@@ -0,0 +1,27 @@
+package com.example.sacrew.numericov4.fragments;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.sacrew.numericov4.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class creditsFragment extends Activity {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_credits, container, false);
+    }
+
+}
--- app/src/main/java/com/example/sacrew/numericov4/fragments/homeFragment.java
+++ app/src/main/java/com/example/sacrew/numericov4/fragments/homeFragment.java
@@ -1,19 +1,25 @@
 package com.example.sacrew.numericov4.fragments;
 
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.sacrew.numericov4.R;
+import com.example.sacrew.numericov4.fragments.customPopUps.popUpFalsePosition;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class homeFragment extends Fragment {
-
+    private Button btnCredits;
+    private View view;
 
     public homeFragment() {
         // Required empty public constructor
@@ -24,7 +30,22 @@ public class homeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        btnCredits = view.findViewById(R.id.btnCredits);
+        btnCredits.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                execute();
+            }
+        });
+        return view;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void execute(){
+        Intent i = new Intent(getContext().getApplicationContext(), creditsFragment.class);
+        startActivity(i);
     }
 
 }
