696e3e07230689e7ae14829750da0eb6dc3ec832
sebastian <sebastianospina51@gmail.com>
2018-06-27 19:12:02 -0500

    removing multipliers section

--- app/src/main/java/com/example/sacrew/numericov4/fragments/systemEquationsFragment/totalPivoting.java
+++ app/src/main/java/com/example/sacrew/numericov4/fragments/systemEquationsFragment/totalPivoting.java
@@ -38,8 +38,6 @@ import static com.example.sacrew.numericov4.fragments.systemEquations.times;
  * A simple {@link Fragment} subclass.
  */
 public class totalPivoting extends baseSystemEquations {
-    private LinearLayout multipliersLayout;
-    private ScrollView scrollview;
 
     public totalPivoting() {
         // Required empty public constructor
@@ -52,7 +50,6 @@ public class totalPivoting extends baseSystemEquations {
         View view = inflater.inflate(R.layout.fragment_total_pivoting, container, false);
         matrixResult = view.findViewById(R.id.matrixResult);
         Button run = view.findViewById(R.id.run);
-        multipliersLayout = view.findViewById(R.id.multipiers);
         Button runHelp = view.findViewById(R.id.runHelp);
         runHelp.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
@@ -87,7 +84,6 @@ public class totalPivoting extends baseSystemEquations {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
-                multipliersLayout.removeAllViews();
                 animatorSet.removeAllListeners();
                 animatorSet.end();
                 animatorSet.cancel();
@@ -97,7 +93,6 @@ public class totalPivoting extends baseSystemEquations {
             }
 
         });
-        scrollview = view.findViewById(R.id.scrollMultipliers);
         return view;
     }
 
@@ -118,36 +113,12 @@ public class totalPivoting extends baseSystemEquations {
             marks[i] = i;
 
         animatorSet = new AnimatorSet();
-        multipliersLayout.removeAllViews();
         animations = new LinkedList<>();
 
         for (int k = 0; k < expandedMatrix.length - 1; k++) {
             final int auxk = k;
             expandedMatrix = totalPivot(k, expandedMatrix, marks);
-            ValueAnimator stage = ValueAnimator.ofInt(0, 1);
-            stage.addListener(new Animator.AnimatorListener() {
-                @Override
-                public void onAnimationStart(Animator animator) {
-                    multipliersLayout.addView(defaultTextView("stage " + auxk, defaultColor, LinearLayout.LayoutParams.MATCH_PARENT, 13));
-                }
 
-                @Override
-                public void onAnimationEnd(Animator animator) {
-                    if (!animations.isEmpty()) animations.remove(0);
-                    scrollview.fullScroll(ScrollView.FOCUS_DOWN);
-                }
-
-                @Override
-                public void onAnimationCancel(Animator animator) {
-
-                }
-
-                @Override
-                public void onAnimationRepeat(Animator animator) {
-
-                }
-            });
-            animations.add(stage);
             for (int i = k + 1; i < expandedMatrix.length; i++) {
                 if (expandedMatrix[k][k] == 0)
                     styleWrongMessage("Error division by 0");
@@ -166,29 +137,6 @@ public class totalPivoting extends baseSystemEquations {
                         } catch (Exception e) {
                             matrixResult.removeAllViews();
                         }
-                    }
-                });
-                colorAnimator.addListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animator) {
-                        multipliersLayout.addView(defaultTextView("multiplier" + (auxi - auxk) + " = " + multiplier, defaultColor, LinearLayout.LayoutParams.MATCH_PARENT, 10));
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animator animator) {
-                        if (!animations.isEmpty()) animations.remove(0);
-                        scrollview.fullScroll(ScrollView.FOCUS_DOWN);
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animator) {
-                        ((TableRow) matrixResult.getChildAt(auxi)).getChildAt(auxk).setBackgroundColor(defaultColor);
-                        ((TableRow) matrixResult.getChildAt(auxk)).getChildAt(auxk).setBackgroundColor(defaultColor);
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animator) {
-
                     }
                 });
                 animations.add(colorAnimator);
